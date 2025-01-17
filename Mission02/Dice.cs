69dc1efd2490c2c1a1a7024bc6368fb6857d3eaa
brandonfromm <156289370+brandonfromm@users.noreply.github.com>
2025-01-17 16:48:33 -0700

    created program and dice classes to simulate dice rolls and displaying the percentage of each number total that is rolled

---
+++ Mission02/Dice.cs
@@ -0,0 +1,24 @@
+namespace Mission2;
+
+public class Dice
+{
+    public int[] RollDice(int numRolls)
+    {
+        // gets random class and creates an array for the dice rolls
+        Random rnd = new Random();
+        int[] diceArray = new int[13];
+        
+        // gets the result of dice rolls for the number of rolls the user inputs
+        for (int i = 0; i < numRolls; i++)
+        {
+            // get dice rolls
+            int diceOne = rnd.Next(1, 7);
+            int diceTwo = rnd.Next(1, 7);
+            int diceTotal = diceOne + diceTwo;
+            
+            // add rolls to the array for the given dice roll result
+            diceArray[diceTotal]++;
+        }
+        return diceArray;
+    }
+}
---
+++ Mission02/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Brandon Fromm
+// Section 003
+// Group 9
+namespace Mission2;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Dice d = new Dice();
+        
+        // display welcome message and asks for number of rolls
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine();
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        
+        // gets number of rolls from input and creates an array for the returned array of RollDice class
+        int numRolls = Convert.ToInt32(Console.ReadLine());
+        int[] rollsArray = d.RollDice(numRolls);
+        
+        // more messages
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numRolls}.");
+        Console.WriteLine();
+        // iterates through each possible roll result
+        for (int i = 2; i <= 12; i++)
+        {
+            // calculates the percent of each roll number and displays the number and the percent in asterisks
+            int dicePercentage = (rollsArray[i] * 100) / numRolls;
+            Console.Write($"{i}: ");
+            Console.WriteLine(new string('*', dicePercentage));
+            
+        }
+        // prints goodbye message
+        Console.WriteLine();
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye");
+    }
+}
